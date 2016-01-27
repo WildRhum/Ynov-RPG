cc276991a62e4c0f8c96244f17f6b64fb4091c11
WildRhum <kyon-04@hotmail.fr>
2016-01-27 14:38:49 +0100

    Changement Script + Camera
    -> Changement du nom des scripts
    -> Ajout du suivi du joueur avec la caméra
    -> La caméra ne doit pas afficher du vide prochainement si possible
    (bloquer la caméra aux extrémités)

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    Camera myCam;
+
+    // Use this for initialization
+    void Start()
+    {
+        myCam = GetComponent<Camera> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myCam.orthographicSize = (Screen.height / 100f) / 2f;
+
+        if (target)
+        {
+            // Déplace la camera à partir de ? , vers ? , a une vitesse de ?, le vecteur ajouté bloque l'axe z 
+            transform.position = Vector3.Lerp(transform.position, target.position, 0.1f) + new Vector3(0, 0, -10);            
+        }
+    }
+}
